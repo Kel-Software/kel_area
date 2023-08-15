23bc0044a75024dc26edfda4ece4f30d74591555
Ahmet Duran <66683435+siriiuss@users.noreply.github.com>
2023-08-15 18:55:19 +0300

    Zero to hero

---
+++ parts/header.php
@@ -0,0 +1,54 @@
+<header class="header-box bg-dark">
+    <div class="header-box-2"></div>
+    <!-- <img class="header-img" src="assets/media/kel-256.png" alt=""> -->
+        
+</header>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+    
+          <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+            <ul class="navbar-nav">
+
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">Projects</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Gallery</a>
+              </li>
+
+            </ul>
+            
+          </div>
+          <!--          
+          <div class="text-end mb-3 mb-lg-0 me-lg-5">
+            <button type="button" class="btn btn-outline-light me-2">Login</button>
+            <button type="button" class="btn btn-warning">Sign-up</button> 
+          </div> -->
+
+        </div>
+      </nav>
+
+      <div class="toast fade show cookies" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header bg-body-secondary">
+          <svg class="bd-placeholder-img rounded me-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#007aff"></rect></svg>
+          <strong class="me-auto">Gizlilik Bildirimi</strong>
+          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body text-black fw-bold">
+          Kimsenin okumadığı zırvalıklar
+        </div>
+      </div>
+<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
